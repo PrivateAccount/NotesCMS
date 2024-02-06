4c45f2883a132834256e89bca427f7b0f8dbbdd5
Andrzej <andrzuk@gmail.com>
2024-02-06 15:33:51 +0100

    Improving admin panel template.

--- application/templates/contents/admin.php
+++ application/templates/contents/admin.php
@@ -2,9 +2,24 @@
 
 $main_template_content = '
 
-	'
-		. $this->get_content() .
-	'
+	<style>
+		section.admin h3.panel-title { margin: 2em; }
+	</style>
+	
+	<table width="100%">
+		<tr>
+			<td>
+				<h3 class="panel-title"><i class="fas fa-tools"></i>&nbsp; Panel administratora</h3>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				'
+					. $this->get_content() .
+				'
+			</td>
+		</tr>
+	</table>
 
 ';
 
